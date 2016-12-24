2b90f200906d42ec232a883dc8b34a92019dba8b
Gustafsson Tom <jkinnala@gmail.com>
2016-12-24 12:50:06 +0200

    Useless work on refine

--- TriangularMesh.m
+++ TriangularMesh.m
@@ -1,18 +1,25 @@
-classdef TriangularMesh
-    %TRIANGULARMESH A collection of triangle elements
-    %   The mesh can be visualized and refined.
+classdef TriangularMesh < handle
+    % TRIANGULARMESH A collection of triangular elements
+    % The mesh can be visualized and refined.
+    %
+    % TRIANGULARMESH Properties:
+    %    p - The points, size 2 x number-of-points
+    %    t - The triangles, size 3 x number-of-elements
+    %
+    % TRIANGULARMESH Methods:
+    %    draw - Visualize the mesh
+    %    refine - Split each triangle into four subtriangles1
     
     properties
-        p % points, size Ndim x Npoints
-        t % triangles, size 3 x Nelems
+        p
+        t
     end
     
     methods
-        %% The constructor
-        % Creates a Mesh object with the specified points and triangles.
-        % If no points and triangles are given, creates a square mesh
-        % with four elements.
         function obj = TriangularMesh(varargin)
+            % Creates a Mesh object with the specified points and triangles.
+            % If no points and triangles are given, creates a square mesh
+            % with four elements.
             if nargin == 0
                 % simple mesh with four elements
                 obj.p = [0 1 1 0 0.5;
@@ -20,15 +27,18 @@ classdef TriangularMesh
                 obj.t = [1 2 3 1;
                          2 3 4 4;
                          5 5 5 5];
-            else
+            elseif nargin == 2
                 obj.p = varargin{1};
-                obj.t = varargin{2};
+                obj.t = sort(varargin{2});
+            else
+                error(['TriangularMesh constructor accepts either two',
+                       ' or zero arguments.']);
             end
         end
         
-        %% Visualize the mesh
-        % Draw the mesh. Return a handle to the plot.
         function h = draw(obj)
+            % Draw the mesh. Return a handle to the plot.
+
             % TODO
             % see help(figure)
             % see help(line)
@@ -42,24 +52,39 @@ classdef TriangularMesh
             %% </MODEL>
         end
         
-        %% Refine the mesh
-        % Split each triangle into four subtriangles.
-        function refine(obj)
-            % TODO
-            %% <MODEL>
-            % create new points: edge midpoints
-            new_p = 0.5*(obj.p(1, :) + obj.p(2, :));
-            new_p = [new_p 0.5*(obj.p(2, :) + obj.p(3, :))];
-            new_p = [new_p 0.5*(obj.p(1, :) + obj.p(3, :))];
-            unique(new_p', 'rows')'
-            % create new triangles
-            Npoints = size(obj.p, 2);
-            new_ix1 = (Npoints+1):(2*Npoints);
-            new_ix2 = (2*Npoints+1):(3*Npoints);
-            new_ix3 = (3*Npoints+1):(4*Npoints);
-            new_t = [obj.t(1, :);
-                     new_ix1
-            %% </MODEL>
+        function obj = refine(obj)
+            % Split each triangle into four subtriangles.
+            edges = [];
+            edges = [edges sort([obj.t(1, :); obj.t(2, :)], 1)];
+            edges = [edges sort([obj.t(2, :); obj.t(3, :)], 1)];
+            edges = [edges sort([obj.t(1, :); obj.t(3, :)], 1)];
+
+            edges = sort(edges, 1);
+       
+            [~, ~, t2e] = unique(edges', 'rows');
+            t2e = t2e(:)';
+
+            t2e = reshape(t2e, size(obj.t, 2), 3)';
+            
+            % refine
+            Np = size(obj.p, 2);
+            
+            for n=1:2
+                e_nodes(n, :) = sum([obj.p(n, edges(1, :)); obj.p(n, edges(2, :))])/2;
+            end
+
+            % Create new mesh
+            new_p = [obj.p e_nodes];
+
+
+            % Edges as n1->n2, n2->n3, n1->n3.
+            new_t = [obj.t(1,:) ; t2e(1,:)+Np ; t2e(3,:)+Np ];
+            new_t = [new_t [obj.t(2,:) ; t2e(1,:)+Np ; t2e(2,:)+Np ]];
+            new_t = [new_t [obj.t(3,:) ; t2e(3,:)+Np ; t2e(2,:)+Np ]];
+            new_t = [new_t [t2e(1,:)+Np ; t2e(2,:)+Np ; t2e(3,:)+Np ]];
+            
+            obj.p = new_p;
+            obj.t = new_t;
         end
     end
     
